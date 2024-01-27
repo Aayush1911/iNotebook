6f1a71b6daf0e58e6e5229b47eb390550cc4ebcb
Aayush1911 <21it49@svitvasad.ac.in>
2024-01-27 21:24:43 +0530

    iNotebook

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="#">Navbar</Link>
+    <Link className="navbar-brand" to="#">iNotebook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
